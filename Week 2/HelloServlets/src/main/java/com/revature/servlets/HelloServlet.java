7b9fd5c2aea071f46c438ef0c6ff560fff6bc41d
gattietime <gattietime@gmail.com>
2020-12-08 12:43:05 -0500

    added helloservlets demo

---
+++ Week 2/HelloServlets/src/main/java/com/revature/servlets/HelloServlet.java
@@ -0,0 +1,30 @@
+package com.revature.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet{
+
+	
+	private static final long serialVersionUID = 1L;
+	
+	/*
+	 * We are overriding the doGet method, so the service method of our HelloServlet will
+	 * delegate here if it receives a GET request. 
+	 * 
+	 * Note the method signature of the doGet method. It will be the same for any of the "do" methods
+	 */
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		//A print writer created from the response will write to that response object. We can use this to write HTML directly
+		PrintWriter pw = res.getWriter();
+		pw.print("<h1>Hello from your doGet method</h1>");
+		res.setStatus(200);
+	}
+
+}

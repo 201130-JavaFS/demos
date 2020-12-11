a9d09f2312e54f246f19f1747e386f9486dbe428
gattietime <gattietime@gmail.com>
2020-12-11 17:25:00 -0500

    avengers demo complete

--- Week 2/AvengersDemo/frontend/avenger.js
+++ Week 2/AvengersDemo/frontend/avenger.js
@@ -2,8 +2,37 @@ const url = 'http://localhost:8080/AvengersDemo/';
 
 document.getElementById("getAvButton").addEventListener('click', assembleFunc);
 
+document.getElementById("loginbtn").addEventListener('click', loginFunc);
+
+async function loginFunc() {
+  let usern = document.getElementById("username").value;
+  let userp = document.getElementById("password").value;
+
+  let user = {
+    username:usern,
+    password:userp
+  };
+
+  let resp = await fetch(url+'login', {
+    method:"POST",
+    body: JSON.stringify(user),
+    credentials: 'include'
+    //Credentials:include will ensure that they cookie is captured, future fetch requests
+    //will also require this value in order to send the cookie back. 
+  });
+
+  if(resp.status===200){
+    document.getElementById('login-row').innerText="YOU HAVE LOGGED IN";  
+    await assembleFunc();
+  }else{
+    document.getElementById('login-row').innerText="Login failed! Reload the page of the computer will explode!"; 
+  }
+
+}
+
+
 async function assembleFunc(){
-  let response = await fetch(url+"avengers");
+  let response = await fetch(url+"avengers", {credentials: 'include'});
 
   if(response.status===200){
     console.log(response);

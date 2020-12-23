0ae979fde79822e6b58c118b2f42f945ddec2ea2
gattietime <gattietime@gmail.com>
2020-12-23 18:03:59 -0500

    fixed AveDemo2 with hashcode

--- Week3/AvengersDemo/src/main/java/com/revature/services/LoginService.java
+++ Week3/AvengersDemo/src/main/java/com/revature/services/LoginService.java
@@ -3,7 +3,7 @@ package com.revature.services;
 public class LoginService {
 
 	public boolean login(String username, String password) {
-		if(username.equals("agent") && password.equals("cellist")) {
+		if(username.equals("agent") && password.equals(((Integer)"cellist".hashCode()).toString())) {
 			return true;
 		}
 		return false;
